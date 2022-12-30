1661aeae0eb4b0509d62e0b67c68487e6f84125c
Leonid Muzyka <leonidmuzika86@gmail.com>
2022-12-30 17:03:22 +0300

    COR-199 add null handling for ne|null, and eq|null

--- src/Scopes/RequestScope.php
+++ src/Scopes/RequestScope.php
@@ -85,9 +85,17 @@ class RequestScope implements Scope
                     } else {
                         $query->orWhere($column, $parsed['operator'], $value);
 
-                        // add all null rows in case <> operator is used.
-                        if(in_array($parsed['operator'], [ '<>', '!=' ])) {
+                        // add all null rows in case ne| is used and value !== null.
+                        // add all null rows in case eq|null
+                        if(
+                            (in_array($parsed['operator'], [ '<>', '!=' ]) && strtolower($value) !== 'null')
+                            || (in_array($parsed['operator'], [ '=' ]) && strtolower($value) === 'null')
+                        ) {
                             $query->orWhereNull($column);
+
+                        // add all not null rows in case ne|null is used
+                        } else if ( in_operator($parsed['operator'], [ '<>', '!=' ]) && strtolower($value) === 'null' ){
+                            $query->orWhereNotNull($column);
                         }
                     }
                 }
